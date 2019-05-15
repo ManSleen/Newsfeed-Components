0e48fb0ce3f806c90abe80afc02364fdd875daa3
Mike VanSleen <mike.vansleen@gmail.com>
2019-05-15 16:02:09 -0600

    Applied animation effect to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,41 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-};
-
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
-console.log(menuButton);
+const body = document.querySelector("body");
+
+console.log(body);
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener("click", () => toggleMenu);
+let tween = TweenMax.from(menu, 0.2, {
+  left: -250,
+  reversed: true,
+  paused: true
+});
+
+const toggleMenu = event => {
+  // Toggle the "menu--open" class on your menu refence.
+  // menu.classList.toggle("menu--open");
+  tween.reversed() ? tween.play() : tween.reverse();
+};
+
+// function closeMenu() {
+//   if (event.target != menu) {
+//     tween.reverse();
+//     console.log("You clicked outside menu!");
+//   }
+// }
+
+menuButton.addEventListener("click", () => toggleMenu());
+
+window.addEventListener("mouseup", function(event) {
+  if (
+    event.target != menu &&
+    event.target.parentNode != menu &&
+    event.target.parentNode.parentNode != menu &&
+    event.target != menuButton
+  ) {
+    tween.reverse();
+    console.log("You clicked outside menu!");
+  }
+});
